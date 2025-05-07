0603010b9347fea838dc00cefb12f4709f50a00b
AbdelrhmanArafat <abdelrhmanarafatahmed@gmail.com>
2025-05-07 23:39:43 +0300

    Stop tracking constants.dart

--- lib/utils/constants/constants.dart
+++
@@ -1,3 +0,0 @@
-  final String placesApiBaseUrl = 'https://maps.googleapis.com/maps/api/place';
-  final String routersApiBaseUrl = 'https://routes.googleapis.com/directions/v2:computeRoutes';
-  final String apiKey = 'AIzaSyB7vumUdy887Papmvig46-yp5Jv9fhgvzc';
